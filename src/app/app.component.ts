3bb53b31ea4e2a3623822cfdb78f44b61c4cad04
v-samoylov <samoylov.vladimir@yahoo.com>
2020-03-01 23:05:20 -0700

    task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,8 +15,12 @@ export class AppComponent {
 
   trackList = [];
 
-  minDragGridWidth = 0;
+  minDragGridWidth = '0';
 
+  /**
+   * Gets coordinates on the hours scale.
+   * @param {number} hourIdx - The index of the hour.
+   */
   getCoordinates(hourIdx) {
     if (this.hoursCoords[hourIdx] !== undefined) return;
     const elem = document.getElementById('hour' + hourIdx);
@@ -25,21 +29,34 @@ export class AppComponent {
     if (hourIdx === 1) this.minDragGridWidth = `${coords.right}px`;
   }
 
-  uploadTrack(event) {
+  /**
+   * Adds music track to the tracklist.
+   * @param {Event} event - File select event.
+   */
+  addTrack(event) {
+    console.log("NEW TRACK", event.srcElement.files[0] );
     this.trackList.push( ...event.srcElement.files );
     event.srcElement.value = '';
   }
 
+  /**
+   * Removes music track from the tracklist.
+   * @param {number} trackIdx - The index of the music track.
+   */
   removeTrack(trackIdx) {
      this.trackList.splice(trackIdx, 1);
   }
 
+  /**
+   * Sets random color for a track.
+   * @param {File} track - The track to which color will be assigned.
+   */
   getRandomColor(track) {
     if (track.color) return track.color;
     var letters = '0123456789ABCDEF';
     var color = '#';
     for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+      color += letters[Math.floor(Math.random() * 16)];
     }
     return track.color = color;
   }
